bca17ff6cbf01dac3523a1927ce978e86d607d24
jefaokpta <jefaokpta@hotmail.com>
2024-06-14 18:33:22 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const { spawnSync } = require('child_process');
+
+
+app.get('/', (req, res) => {
+    transcribe('momoa.ogg')
+    res.sendStatus(200)
+});
+
+app.listen(port, () => {
+    console.log(`🚀 App listening at http://localhost:${port}`);
+});
+
+function transcribe(audioName) {
+    const command = 'venv/bin/whisper '
+        + audioName + ' ' +
+        '--model=large ' +
+        '--fp16=False ' +
+        '--language=pt ' +
+        '--beam_size=5 ' +
+        '--patience=2 ' +
+        '--output_format=json'
+    const result = spawnSync(command, { shell: true });
+    console.log(result.status)
+}

1a5b98322cc2d45a39ff6f3b65496028e9532da1
jefaokpta <jefaokpta@hotmail.com>
2024-08-30 17:00:48 -0300

    perfeitamente integrado a veia

--- src/main.js
+++ src/main.js
@@ -1,42 +1,59 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 const {spawnSync} = require('child_process');
-const {readFile, unlink} = require('fs');
+const {unlink, createWriteStream, readFileSync} = require('fs');
+const axios = require("axios");
+
 
 app.use(express.json());
 
-app.post('/', (req, res) => {
-    console.log('Transcrevendo audio: ', req.body)
+const S3_MEDIA_PATH = process.env.S3_MEDIA_PATH
+const LOCAL_AUDIO_PATH = 'audios/';
+
+app.post('/transcribe', async (req, res) => {
+    console.log('Transcrevendo audio do S3: ', req.body)
     const audio = req.body.audio;
-    const transcribeStatus = transcribe(audio);
-    if (transcribeStatus !== 0) {
-        res.status(500).send('Erro ao transcrever o áudio');
-        return
+    const controlNumber = req.body.controlNumber;
+    const writer = createWriteStream(LOCAL_AUDIO_PATH + audio);
+    const transcriptionFileJs = audio.substring(0, audio.lastIndexOf('.')) + '.json';
+
+    try{
+        const response = await axios({
+            method: 'GET',
+            url: S3_MEDIA_PATH + `/${controlNumber}/${audio}`,
+            responseType: 'stream'
+        })
+        response.data.pipe(writer);
+    } catch (e){
+        console.error(e, e.message, 'Erro ao baixar arquivo de audio do S3.')
+        res.status(500).send('Error downloading the audio file.');
     }
-    const transcriptionFile = audio.substring(0, audio.lastIndexOf('.')) + '.json';
-    readFile(transcriptionFile, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        unlink(transcriptionFile, (err) => {
-            if (err) {
-                console.error(err)
-            }
-        })
-        unlink('audios/' + audio, (err) => {
-            if (err) {
-                console.error(err)
-            }
-        })
-        res.json(JSON.parse(data.toString()))
-    })
+
+
+    writer.on('finish', () => {
+        res.json(prepareToTranscribe(audio, transcriptionFileJs));
+        cleanFileAndMedia(audio, transcriptionFileJs);
+    });
+
+    writer.on('error', (err) => {
+        console.error(err);
+        res.status(500).send('Error saving the audio file.');
+    });
 });
 
 app.listen(port, () => {
     console.log(`🚀 App listening at http://localhost:${port}`);
 });
+
+function prepareToTranscribe(audioName, transcriptionFileJs) {
+    const transcribeStatus = transcribe(audioName);
+    if (transcribeStatus !== 0) {
+        throw new Error('Erro ao transcrever audio.');
+    }
+    return JSON.parse(readFileSync(transcriptionFileJs, 'utf8'))
+}
 
 function transcribe(audioName) {
     const command = 'venv/bin/whisper '
@@ -49,4 +66,17 @@ function transcribe(audioName) {
         '--output_format=json'
     const result = spawnSync(command, { shell: true });
     return result.status
+}
+
+function cleanFileAndMedia(audioName, transcriptionFileJs) {
+    unlink(transcriptionFileJs, (err) => {
+        if (err) {
+            throw new Error('Erro ao deletar arquivo de transcrição.');
+        }
+    })
+    unlink(LOCAL_AUDIO_PATH + audioName, (err) => {
+        if (err) {
+            throw new Error('Erro ao deletar arquivo de audio.');
+        }
+    })
 }
